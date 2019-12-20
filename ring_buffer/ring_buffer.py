d239609580760e5f656998ae9266ce931ef9c81e
Lambda_School_Loaner_163 <kimberlyswinton@gmail.com>
2019-12-20 14:52:04 -0500

    :white_check_mark: ring_buffer is done
    ring_buffer is done

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -8,18 +8,38 @@ class RingBuffer:
         self.storage = DoublyLinkedList()
 
     def append(self, item):
-        if len(self.storage) == self.capacity:
-            if self.current is None:
-                self.current = self.storage.tail
-            self.current.value = item
-            if self.current.prev:
-                self.current = self.current.prev
-            else: self.current = self.storage.tail
-        else: self.storage.add_to_head(item)
+        if self.storage.length < self.capacity:
+            self.storage.add_to_head(item)
+            self.current = self.storage.tail
+        elif self.current is self.storage.tail:
+            self.storage.remove_from_tail()
+            self.storage.add_to_tail(item)
+            self.current = self.current.prev
+        elif self.current is self.storage.head:
+            self.storage.remove_from_head()
+            self.storage.add_to_head(item)
+            self.current = self.storage.tail
+        else:
+            self.current.insert_after(item)
+            self.current.delete()
+            self.current = self.current.prev
 
-    #WORKING ON IT!
+    def get(self):
+        # Note:  This is the only [] allowed
+        list_buffer_contents = []
 
-    
+        # TODO: Your code here ####
+        
+        current = self.storage.head
+        while current != None:
+          list_buffer_contents.insert(0, current.value)
+          if current.next == None:
+            break
+          else:
+            current = current.next
+
+        return list_buffer_contents
+
 
 # ----------------Stretch Goal-------------------
 
